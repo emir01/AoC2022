62db910481d32de311f1194de322b7acb5060f60
Emir Osmanoski <emir.osmanoski@gmail.com>
2022-12-06 23:20:03 +0100

    Day6 - Part 1

---
+++ AdventOfCode/Day06.cs
@@ -0,0 +1,75 @@
+using AdventOfCode.Utils;
+
+namespace AdventOfCode
+{
+    public class Day06 : BaseDay
+    {
+        private readonly string _input;
+
+        public Day06()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            // process the input string
+            var logger = new LogWrapper();
+
+            var markerIndex = 0;
+
+            logger.WriteLine("=======  PART 1 =======");
+
+            logger.WriteLine(_input);
+
+            markerIndex = FindIndexForUniqueLengthMarker(_input, 4, logger);
+
+            return new(markerIndex.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var logger = new LogWrapper();
+            
+            // process the input string
+            var score = "";
+
+            logger.WriteLine("=======  PART 2 =======");
+
+            return new(score);
+        }
+
+        private int FindIndexForUniqueLengthMarker(string input, int markerLength, LogWrapper logger)
+        {
+            var markerIndex = 0;
+            var tempString = "";
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                tempString += input[i];
+                if (tempString.Length > 4)
+                {
+                    tempString = tempString.Substring(1, tempString.Length - 1);
+                }
+
+                // if we are finally dealing with a string of length 4 - check if all characters are unique
+
+                if (tempString.Length == markerLength)
+                {
+                    logger.WriteLine($"Checking if {tempString} is all unique characters!");
+
+                    var allUniqueChars =
+                        tempString.All(workChar => tempString.Count(otherChar => otherChar == workChar) == 1);
+
+                    if (allUniqueChars)
+                    {
+                        markerIndex = i + 1;
+                        break;
+                    }
+                }
+            }
+
+            return markerIndex;
+        }
+    }
+}
