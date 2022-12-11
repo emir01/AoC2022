5cb36dffe2f2bbf3346620435c5b033ed52c4cf0
Emir Osmanoski <emir.osmanoski@gmail.com>
2022-12-11 01:12:14 +0100

    Day10 - Part 2 - Initial Skeleton + First Line Drawn?

--- AdventOfCode/Day10.cs
+++ AdventOfCode/Day10.cs
@@ -39,13 +39,61 @@ public class Day10 : BaseDay
         }
     }
 
+    public class Crt
+    {
+        public List<string> Pixels { get; set; }
+
+        public Crt()
+        {
+            Pixels = Enumerable.Range(1, 240).Select(x => ".").ToList();
+        }
+
+        public void PrintCrt(LogWrapper logger)
+        {
+            logger.WriteLine("================CRT START================");
+            var drawnPixelCount = 0;
+            for (int i = 0; i < Pixels.Count; i++)
+            {
+                logger.Write(Pixels[i]);
+
+                drawnPixelCount++;
+
+                if (drawnPixelCount == 40)
+                {
+                    drawnPixelCount = 0;
+                    logger.Write("\n");
+                }
+            }
+
+            logger.WriteLine("================CRT END==================");
+        }
+
+        public void UpdateAfterCycle(int cycle, int stateRegister, LogWrapper logger)
+        {
+            // check what needs to be drawn
+            logger.WriteLine($"At Cycle {cycle} register is Set at: {stateRegister}");
+
+            if (stateRegister == cycle || stateRegister + 1 == cycle || stateRegister - 1 == cycle)
+            {
+                logger.WriteLine("DRAWING");
+                Pixels[cycle - 1] = "#";
+            }
+            else
+            {
+                logger.WriteLine("NOT DRAWING");
+            }
+        }
+    }
+
     private class CpuState
     {
         public int Cycle { get; set; }
 
         public List<CpuInstruction> LongRunningInstructions { get; set; }
 
-        public int Register { get; set; }
+        public int RegisterAtEndOfCycle { get; set; }
+
+        public int RegisterAtStartOfCycle { get; set; }
 
         public int[] CyclesToCheckStrengths { get; set; }
 
@@ -57,12 +105,22 @@ public class Day10 : BaseDay
 
         public CpuState(int[] cyclesToCheck)
         {
+            if (cyclesToCheck == null)
+            {
+                CyclesToCheck = new int[] { };
+            }
+            else
+            {
+                CyclesToCheck = cyclesToCheck;
+            }
+
             Cycle = 0;
-            Register = 1;
+            RegisterAtEndOfCycle = 1;
+            RegisterAtStartOfCycle = 1;
+
 
             // Cycles to Check
-            CyclesToCheck = cyclesToCheck;
-            CyclesToCheckStrengths = Enumerable.Repeat(0, cyclesToCheck.Length).ToArray();
+            CyclesToCheckStrengths = Enumerable.Repeat(0, CyclesToCheck.Length).ToArray();
 
             InstructionPointer = 0;
             CheckCycleIndex = 0;
@@ -84,7 +142,7 @@ public class Day10 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         // process the input string
-        var logger = new LogWrapper();
+        var logger = new LogWrapper(false);
 
         logger.WriteLine("===== PART 1 =====");
 
@@ -93,11 +151,9 @@ public class Day10 : BaseDay
             20, 60, 100, 140, 180, 220
         };
 
-        var stateAfterInstructions = RunInstructionsWithCheckCycles(cyclesToCheck, logger);
+        var stateAfterInstructions = RunInstructionsWithCheckCycles(logger, cyclesToCheck);
 
-        logger.WriteLine($"Cycle Strengths: {JsonSerializer.Serialize(stateAfterInstructions.CyclesToCheckStrengths)}");
-
-        var sum = stateAfterInstructions.CyclesToCheckStrengths.Sum();
+        var sum = stateAfterInstructions.Last().CyclesToCheckStrengths.Sum();
 
         return new(sum.ToString());
     }
@@ -105,23 +161,35 @@ public class Day10 : BaseDay
     public override ValueTask<string> Solve_2()
     {
         // process the input string
-        var logger = new LogWrapper(false);
+        var logger = new LogWrapper();
 
         logger.WriteLine("===== PART 2 =====");
 
-        return new("Part 2");
+        var crt = new Crt();
+
+        logger.WriteLine($"CRT BEFORE Program Execution");
+        crt.PrintCrt(logger);
+        var cpuCyclesEnumerable = RunInstructionsWithCheckCycles(logger);
+        foreach (var state in cpuCyclesEnumerable)
+        {
+            crt.UpdateAfterCycle(state.Cycle, state.RegisterAtStartOfCycle, logger);
+        }
+
+        logger.WriteLine($"CRT After Program Execution");
+        crt.PrintCrt(logger);
+
+        return new("ABCDEFG");
     }
 
-    private CpuState RunInstructionsWithCheckCycles(int[] cyclesToCheck, LogWrapper logger)
+    private IEnumerable<CpuState> RunInstructionsWithCheckCycles(LogWrapper logger, int[] cyclesToCheck = null)
     {
         CpuState state = new CpuState(cyclesToCheck);
-
-        logger.WriteLine($"Dealing with {_instructions.Count} instructions");
 
         // Start running the processor
         while (true)
         {
             state.Cycle++;
+            state.RegisterAtStartOfCycle = state.RegisterAtEndOfCycle;
 
             // check if we have reached a cycle for which we need to check strength
             if (state.CheckCycleIndex < state.CyclesToCheck.Length &&
@@ -130,14 +198,12 @@ public class Day10 : BaseDay
                 logger.WriteLine($"Have hit Cycle: {state.Cycle} after {state.InstructionPointer} instructions");
 
                 state.CyclesToCheckStrengths[state.CheckCycleIndex] =
-                    state.Register * state.CyclesToCheck[state.CheckCycleIndex];
+                    state.RegisterAtEndOfCycle * state.CyclesToCheck[state.CheckCycleIndex];
 
                 state.CheckCycleIndex++;
             }
 
             // if we executing adds we look at completing them
-            // todo: This might be an issue if we get to execute multiple instructions per cycle
-            // todo: we might need to use a FIFO queue 
             if (state.LongRunningInstructions.Any())
             {
                 foreach (var executingAdd in state.LongRunningInstructions)
@@ -145,13 +211,14 @@ public class Day10 : BaseDay
                     executingAdd.CyclesLeft--;
                     if (executingAdd.CyclesLeft == 0)
                     {
-                        if (executingAdd.Argument != null) state.Register += executingAdd.Argument.Value;
+                        if (executingAdd.Argument != null) state.RegisterAtEndOfCycle += executingAdd.Argument.Value;
                     }
                 }
 
                 // remove the ones that are finished
                 state.LongRunningInstructions = state.LongRunningInstructions.Where(x => x.CyclesLeft != 0).ToList();
             }
+
             else
             {
                 // if we've reached all instructions we need to execute we break;
@@ -173,8 +240,9 @@ public class Day10 : BaseDay
                     });
                 }
             }
-        }
 
-        return state;
+            // we return the state at the end of the cycle
+            yield return state;
+        }
     }
 }
